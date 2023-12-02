ca6ed23f05a76deba17bbc15be55690ba9641251
Oyaki122 <oc.yaki122@gmail.com>
2023-12-02 10:54:07 +0900

    raspi色々変更

---
+++ messenger/run.sh
@@ -0,0 +1,2 @@
+# !/bin/sh
+poetry run python pub.py --endpoint a5z8x43l4onn6-ats.iot.us-east-1.amazonaws.com --root-ca AmazonRootCA1.pem --cert  5e3297c4ad24d54d1537aae41927a72dd2c45e586bea00205032ed40426e15d3-certificate.pem.crt --key 5e3297c4ad24d54d1537aae41927a72dd2c45e586bea00205032ed40426e15d3-private.pem.key
