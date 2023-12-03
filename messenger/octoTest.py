47f0ade324a029b97d6433a26d9503383f002d20
Oyaki <oc.yaki122@gmail.com>
2023-12-03 18:13:32 +0900

    動いた

--- messenger/octoTest.py
+++ messenger/octoTest.py
@@ -1,49 +1,15 @@
 from octorest import OctoRest
+import requests
 
 url = "http://klipperO.local"
 
-
-def make_client():
-    try:
-        client = OctoRest(url=url, apikey="123")
-        if client is None:
-            raise Exception("client is None")
-        return client
-    except Exception as e:
-        print(e)
-        exit(1)
-
-
-def get_version():
-    client = make_client()
-    message = "You are using OctoPrint v" + client.version['server'] + "\n"
-    return message
-
-
-def get_printer_info():
-    try:
-        client = OctoRest(url=url, apikey="YouShallNotPass")
-        message = ""
-        message += str(client.version) + "\n"
-        message += str(client.job_info()) + "\n"
-        printing = client.printer()['state']['flags']['printing']
-        if printing:
-            message += "Currently printing!\n"
-        else:
-            message += "Not currently printing...\n"
-        return message
-    except Exception as e:
-        print(e)
-
-
 def main():
-    c = make_client()
-    # for k in c.files()['files']:
-        # print(k['name'])
-    # c.home()
 
-    # with open("bency.gcode") as f:
-    print(c.upload("bency.gcode", print=True))
+    file = {
+        "file": ("bency.gcode", open("messenger/bency.gcode", "rb")),
+        "print": "true"
+    }
+    requests.post(url + "/api/files/local", files=file)
 
 
 if __name__ == "__main__":
