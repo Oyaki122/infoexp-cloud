d0ea2fd26021949716a748ec2201e7c0f37cd42c
Oyaki <oc.yaki122@gmail.com>
2023-12-04 02:34:11 +0900

    遠隔プリント実装

--- messenger/pub.py
+++ messenger/pub.py
@@ -7,7 +7,8 @@ from awsiot import mqtt_connection_builder
 import sys
 import threading
 from uuid import uuid4
-
+import json
+import requests
 # This sample uses the Message Broker for AWS IoT to send and receive messages
 # through an MQTT connection. On startup, the device connects to the server,
 # subscribes to a topic, and begins publishing messages to that topic.
@@ -17,29 +18,32 @@ from uuid import uuid4
 parser = argparse.ArgumentParser(
     description="Send and receive messages through and MQTT connection.")
 parser.add_argument(
-    '--endpoint', required=True,
+    '--endpoint',
+    required=True,
     help="Your AWS IoT custom endpoint, not including a port. " +
     "Ex: \"abcd123456wxyz-ats.iot.us-east-1.amazonaws.com\"")
-parser.add_argument(
-    '--port', type=int,
-    help="Specify port. AWS IoT supports 443 and 8883.")
+parser.add_argument('--port',
+                    type=int,
+                    help="Specify port. AWS IoT supports 443 and 8883.")
 parser.add_argument(
     '--cert', help="File path to your client certificate, in PEM format.")
-parser.add_argument(
-    '--key', help="File path to your private key, in PEM format.")
+parser.add_argument('--key',
+                    help="File path to your private key, in PEM format.")
 parser.add_argument(
     '--root-ca',
     help="File path to root certificate authority, in PEM format. " +
     "Necessary if MQTT server uses a certificate that's not already in " +
     "your trust store.")
+parser.add_argument('--client-id',
+                    default="test-" + str(uuid4()),
+                    help="Client ID for MQTT connection.")
+parser.add_argument('--topic',
+                    default="test/topic",
+                    help="Topic to subscribe to, and publish messages to.")
 parser.add_argument(
-    '--client-id', default="test-" +
-    str(uuid4()), help="Client ID for MQTT connection.")
-parser.add_argument(
-    '--topic', default="test/topic",
-    help="Topic to subscribe to, and publish messages to.")
-parser.add_argument(
-    '--use-websocket', default=False, action='store_true',
+    '--use-websocket',
+    default=False,
+    action='store_true',
     help="To use a websocket instead of raw mqtt. If you " +
     "specify this option you must specify a region for signing.")
 parser.add_argument(
@@ -47,11 +51,10 @@ parser.add_argument(
     default='us-east-1',
     help="If you specify --use-web-socket, this " +
     "is the region that will be used for computing the Sigv4 signature")
-parser.add_argument(
-    '--verbosity',
-    choices=[x.name for x in io.LogLevel],
-    default=io.LogLevel.NoLogs.name,
-    help='Logging level')
+parser.add_argument('--verbosity',
+                    choices=[x.name for x in io.LogLevel],
+                    default=io.LogLevel.NoLogs.name,
+                    help='Logging level')
 
 # Using globals to simplify sample code
 args = parser.parse_args()
@@ -95,6 +98,22 @@ def on_resubscribe_complete(resubscribe_future):
 # Callback when the subscribed topic receives a message
 def on_message_received(topic, payload, dup, qos, retain, **kwargs):
     print("Received message from topic '{}': {}".format(topic, payload))
+    data = json.loads(payload)
+
+    try:
+        url = data["url"]
+        filename = data["filename"]
+    except KeyError:
+        print("KeyError")
+        return
+    urlData = requests.get(url).content
+    file = {"file": (filename, urlData), "print": "true"}
+    try:
+        print(
+            requests.post("http://klipperO.local/api/files/local",
+                          files=file).content)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
