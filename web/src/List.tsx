527ba088251f0b2d30a3de5c33ab8ca4e642bce2
Oyaki122 <oc.yaki122@gmail.com>
2023-12-04 11:46:41 +0900

    アップロードを実装

--- web/src/List.tsx
+++ web/src/List.tsx
@@ -1,3 +1,52 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Box } from "@mui/material";
+import axios from "axios";
+import { useEffect } from "react";
+
 export default function List() {
-	return <div>List</div>;
+	useEffect(() => {
+		axios
+			.get("https://qg6wdvbjvh.execute-api.us-east-1.amazonaws.com/1/listFiles")
+			.then((res) => console.log(res))
+			.catch((err) => console.error(err));
+	}, []);
+	return (
+		<Box sx={{ width: "100%" }}>
+			<TableContainer component={Paper}>
+				<Table sx={{ minWidth: 650 }} aria-label="simple table">
+					<TableHead>
+						<TableRow>
+							<TableCell>Dessert (100g serving)</TableCell>
+							<TableCell align="right">Calories</TableCell>
+							<TableCell align="right">Fat&nbsp;(g)</TableCell>
+							<TableCell align="right">Carbs&nbsp;(g)</TableCell>
+							<TableCell align="right">Protein&nbsp;(g)</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{rows.map((row) => (
+							<TableRow
+								key={row.name}
+								sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+							>
+								<TableCell component="th" scope="row">
+									{row.name}
+								</TableCell>
+								<TableCell align="right">{row.calories}</TableCell>
+								<TableCell align="right">{row.fat}</TableCell>
+								<TableCell align="right">{row.carbs}</TableCell>
+								<TableCell align="right">{row.protein}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</Box>
+	);
 }
--- web/src/Upload.tsx
+++ web/src/Upload.tsx
@@ -1,7 +1,8 @@
-import { Button, Stack, TextField, styled } from "@mui/material";
+import { Button, Stack, TextField, Typography, styled } from "@mui/material";
 import Box from "@mui/material/Box";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import { createRef, useState } from "react";
+import axios from "axios";
 
 const VisuallyHiddenInput = styled("input")({
 	clip: "rect(0 0 0 0)",
@@ -17,8 +18,30 @@ const VisuallyHiddenInput = styled("input")({
 
 export default function Upload() {
 	const fileInputRef = createRef<HTMLInputElement>();
-  const [filename, setFilename] = useState("");
-	const send: React.MouseEventHandler = () => {};
+	const [user, setUser] = useState("hoge");
+	const [filename, setFilename] = useState("");
+
+	const createFormData = async () => {
+		const file = fileInputRef.current?.files?.[0];
+		if (!file) throw new Error("ファイルが選択されていません");
+		const formData = new FormData();
+		formData.append("file", file);
+		formData.append("user", user);
+		return formData;
+	};
+	const send: React.MouseEventHandler = async () => {
+		const formData = await createFormData();
+		const res = await axios.post(
+			"https://qg6wdvbjvh.execute-api.us-east-1.amazonaws.com/1/uploadFiles",
+			formData,
+			{
+				headers: {
+					"Content-Type": "multipart/form-data",
+				},
+			},
+		);
+		console.log(res);
+	};
 
 	return (
 		<Box
@@ -34,18 +57,34 @@ export default function Upload() {
 					maxWidth: "500px",
 				}}
 			>
-				<TextField required id="outlined-required" label="ユーザー名" />
-				<Button
-					component="label"
-					variant="contained"
-					startIcon={<CloudUploadIcon />}
-				>
-					ファイル選択
-					<VisuallyHiddenInput type="file" ref={fileInputRef} accept=".gcode"
-            onChange={(e) => e.} 
-          />
-
-				</Button>
+				<TextField
+					required
+					id="outlined-required"
+					label="ユーザー名"
+					value={user}
+					onChange={(e) => setUser(e.currentTarget.value)}
+				/>
+				<Box sx={{ display: "flex", flexDirection: "row" }}>
+					<Button
+						component="label"
+						variant="contained"
+						startIcon={<CloudUploadIcon />}
+					>
+						ファイル選択
+						<VisuallyHiddenInput
+							type="file"
+							ref={fileInputRef}
+							accept=".gcode"
+							onChange={(e) =>
+								e.currentTarget.files &&
+								setFilename(e.currentTarget.files[0].name)
+							}
+						/>
+					</Button>
+					<Box>
+						<Typography>{filename}</Typography>
+					</Box>
+				</Box>
 				<Button variant="contained" onClick={send}>
 					アップロード
 				</Button>
